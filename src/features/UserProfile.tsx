9878e0cda8d68b1bdddb9e19d3b4e6bd9583b9d3
paifuiii <63070014@kmitl.ac.th>
2023-12-24 03:27:58 +0700

    notification with button in suggestion Tab :art:

--- src/features/UserProfile.tsx
+++ src/features/UserProfile.tsx
@@ -87,11 +87,12 @@ export default function UserProfile() {
           onOk={handleModal}
           onCancel={handleModal}
           width={630}
+          footer={null}
           style={{
             borderRadius: "1.25rem",
           }}
         >
-          <div className="flex flex-col space-y-5 h-96">
+          <div className="flex flex-col space-y-5 h-[30rem]">
             <div className="w-full text-[#4C577C] text-lg">Notification</div>
             <Divider className="m-0" />
             <div className="flex text-[#4C577C] space-x-7">
@@ -129,6 +130,35 @@ export default function UserProfile() {
                   </List.Item>
                 )}
               />
+
+
+
+              {/* noti with button in Sugggestion Tab */}
+              <div className="flex p-4 jura hover:bg-[#f2f1f1]">
+                <div className="flex h-16">
+                  <img src={UnreadIcon} className="w-8" alt="" />
+                </div>
+                <div className="flex p-4 space-x-1">
+                  <div className="flex flex-col">
+                    <div className="flex">
+                      <p className="">Dr.Prasert has sent you a message</p>
+                      <p className="text-[#61708C]">Consult #6643793</p>
+                    </div>
+                    <p className="text-[#908F8F]">1 hour ago</p>
+                    <div className="flex space-x-3 mt-3">
+                      <div className=" px-5 text-[.8rem] bg-[#D2D4EB] text-[#4C577C] border-[#8087A7] border-2 rounded">
+                        Accept
+                      </div>
+                      <div className="px-5 text-[.8rem] rounded border-2 border-[#8087A7] ">
+                        Decline
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+
             </div>
           </div>
         </Modal>
