485bd7c32e9edbb2ca90f3d67446bd0811f829c6
paifuiii <63070014@kmitl.ac.th>
2024-02-12 14:36:30 +0700

    feat : edit loop image final :art:

--- src/components/Compare/CardImage.tsx
+++ src/components/Compare/CardImage.tsx
@@ -3,6 +3,7 @@ import { IEquipment, IImage, INote } from "@constants";
 import { formatDate, formatImage } from "@utils";
 import { Tag, Divider, Image } from "antd";
 import { useEffect, useState } from "react";
+import {httpAPI} from "@config";
 
 interface CardImageProps {
   image: IImage;
@@ -46,14 +47,18 @@ export default function CardImage({ image, equipment }: CardImageProps) {
               </div>
               <Divider></Divider>
             </div>
-            <p className="bg-[#F2F2F2] rounded-sm">{item.note_desc}</p>
-            {item.note_img ? (
-              <div className="flex flex-grow gap-2">
-                <Image src={item.note_img} width={44} />
-              </div>
-            ) : (
-              ""
-            )}
+            <p className="bg-[#f6f6f6] rounded-sm p-2">{item.note_desc}</p>
+            <div className="flex gap-3">
+                  {item.note_img.map((image, index) => (
+                    <Image
+                      key={index}
+                      width={80}
+                      height={80}
+                      src={`${httpAPI}/${image}`}
+                      className="rounded-md"
+                    />
+                  ))}
+                </div>
           </div>
         ))}
     </div>
